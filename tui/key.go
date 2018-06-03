d18217cced5dc5b6f6486f18e1f995cb1044a7f3
TcM1911 <mr.cableman@gmail.com>
2018-06-03 15:01:37 +0100

    Update

--- tui/key.go
+++ tui/key.go
@@ -1,3 +1,20 @@
+// Copyright (c) 2018 Joakim Kennedy
+//
+// This file is part of Jamsonic.
+//
+// Jamsonic is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Jamsonic is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Jamsonic.  If not, see <http://www.gnu.org/licenses/>.
+
 package tui
 
 import (
